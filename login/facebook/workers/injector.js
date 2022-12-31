a2e3b33db2308d1fe176724682f127198babe0fa
Hempreet Singh <hempreetsingh123@gmail.com>
2022-12-31 18:50:00 +0530

    updated readme for all the login websites

--- login/facebook/workers/injector.js
+++ login/facebook/workers/injector.js
@@ -1,21 +1,12 @@
-var username;
-var password;
-
-fetch("http://192.168.137.1:8080/creds.json")
-    .then((response) => response.json())
-    .then((json) => {
-        console.log(json);
-        username = json.username;
-        password = json.password;
-    });
-
-
-console.log(username);
-console.log(password);
+var username = "Your Username";
+var password = "Your Password";
 
 document.getElementById("login_field").value=username;
 document.getElementById("password").value=password;
 
-// document.querySelector('.btn.btn-primary.btn-block.js-sign-in-button').click();
+document.querySelector('.btn.btn-primary.btn-block.js-sign-in-button').click();
+
+console.log(username);
+console.log(password);
 
 alert(username);
